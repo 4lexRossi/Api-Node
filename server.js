8864eb1b112f91ba9d14c5c36fc297d4b79e4db6
4lexRossi <financeiro.alexsander@gmail.com>
2020-06-10 22:01:57 -0300

    Api e Front

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require ('express');
+const cors = require ('cors');
+const mongoose = require ('mongoose');
+const requireDir = require ('require-dir');
+
+// iniciando o App
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+// iniciando o DB
+mongoose.connect('mongodb://localhost:27017/nodeapi',
+{ useNewUrlParser: true,
+useUnifiedTopology: true });
+
+//adicionando arquivos nas rotas
+requireDir('./src/models');
+
+
+// rotas
+app.use('/api', require('./src/routes'));
+
+app.listen(3001);
